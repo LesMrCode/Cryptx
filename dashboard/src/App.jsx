6d5344e0c483e58eb16a8997f39016e7ed702cf0
LesMrCode <mhonelesleyeric@gmail.com>
2025-11-05 21:05:46 +0200

    components created

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,1 +1,25 @@
-import { useState } from 'react'
+import React from 'react'
+import Sidebar from "./components/layout/Sidebar.jsx";
+import Header from "./components/layout/Header.jsx";
+import Maincontent from "./components/layout/Maincontent.jsx"; 
+import MetricCard from "./components/ui/MetricCard.jsx";
+import ChartVisual from "./components/ui/ChartVisual.jsx";
+import TransactionTable from "./components/ui/TransactionTable.jsx"; 
+
+export default function App() {
+  return(
+    <div className='flex'>
+      <Sidebar />
+      <div className='flex-1'>
+      <Header />
+
+      <main> 
+      <Maincontent />
+      <MetricCard />
+      <ChartVisual />
+      <TransactionTable />
+      </main>
+      </div>
+    </div>
+  )
+}
